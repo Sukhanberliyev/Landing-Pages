60232ebfe444f90a54f0ab70744b8f5a16083e41
Aidar Sukhanberliyev <68350747+Sukhanberliyev@users.noreply.github.com>
2021-11-15 16:55:19 +0900

    Add Hero Section

--- construction-company/src/components/Hero/Hero.js
+++ construction-company/src/components/Hero/Hero.js
@@ -1,19 +1,27 @@
 import React from "react";
 
+import "./Hero.css";
+import Container from "../Layout/Container";
+
 const Hero = () => {
   return (
-    <section id="hero">
-      <img src="" alt="" />
-      <h1>
-        We <span>build</span> the future
-      </h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et tortor
-        gravida non congue tortor dictum elementum. Nullam risus, ullamcorper
-        elit lobortis gravida. Dolor amet sit pulvinar volutpat vestibulum
-        tortor, tortor dolor accumsan. Magna risus sed velit venenatis neque.
-        Dui egestas nulla sagittis facilisis orci, egestas tellus, morbi amet.{" "}
-      </p>
+    <section className="hero">
+    <img src="/assets/images/heroImg.png" alt="" />
+      <Container>
+        <div className="heroText">
+          <h1>
+            We <span>build</span> the future
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et tortor
+            gravida non congue tortor dictum elementum. Nullam risus,
+            ullamcorper elit lobortis gravida. Dolor amet sit pulvinar volutpat
+            vestibulum tortor, tortor dolor accumsan. Magna risus sed velit
+            venenatis neque. Dui egestas nulla sagittis facilisis orci, egestas
+            tellus, morbi amet.
+          </p>
+        </div>
+      </Container>
     </section>
   );
 };
